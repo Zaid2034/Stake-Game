520b1d35f9c131f6c414a668183b49f938b85e85
zaid <mdzaidali2034@gmail.com>
2024-08-01 00:42:45 +0530

    Add vercel file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(0)
-  axios.defaults.baseURL = 'http://localhost:3000/';
+  axios.defaults.baseURL = 'https://stake-game-backend.vercel.app/';
   return (
     <>
      {/* <Simulation/> */}
